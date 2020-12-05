5a86b27814a340171ac1a08c13df285be1114b43
Murat Altınpınar <64796784+murtekbey@users.noreply.github.com>
2020-12-06 02:08:55 +0300

    3. video tamamlandi

--- page/admin.py
+++ page/admin.py
@@ -3,4 +3,28 @@ from django.contrib import admin
 from django.db import models
 from .models import Page
 
-admin.site.register(Page)
+class PageModify(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'status',
+        'updated_at',
+)
+    list_filter = ('status',)
+    list_editable = (
+        'status',
+        'title',
+)
+
+
+admin.site.register(Page, PageModify)
+
+# M Db yapisi
+# V View / Control
+# T Template
+# A Admin
+# F Forms
+# M Message
+# S Session
--- page/models.py
+++ page/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.utils.text import slugify
 
 STATUS = [
+    # sol kısım dbye yazılacak olan
+    # sağ kısım kullanıcıya gözükecek
+    # Page.objects.filter(status="draft").count()
     ('draft', 'Taslak'),
     ('published', 'Yayınlandi'),
     ('deleted', 'Silindi'),
@@ -12,11 +16,18 @@ class Page(models.Model):
     # title : Hakkımızda / İletişim
     title = models.CharField(max_length=200)
     # slug : (hakkimizda, iletisim) sadece ilk create sirasinda olusmalidir
-
+    slug = models.SlugField(
+        max_length=200,
+        default="",
+        )
     # content : İçerik
     content = models.TextField()
     # cover_image
-    cover_image = models.ImageField(upload_to='page')
+    cover_image = models.ImageField(
+        upload_to='page',
+        null = True,
+        blank = True,
+        )
     # status
     status = models.CharField(
         default="draft",
